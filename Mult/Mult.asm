45e5c46b2e8f1ed873da5d9d01d7da85b91cc701
Cloude <yunsong2023uk@163.com>
2024-09-10 01:58:21 +0930

    add Assignment

--- Mult/Mult.asm
+++ Mult/Mult.asm
@@ -1,61 +1,34 @@
-// This file is based on part of www.nand2tetris.org
+// This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
-// File name: Mult.asm
+// File name: projects/04/Mult.asm
 
-// Multiplies R1 and R2 and stores the result in R0.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
-
-
-    // Initialize R0 to 0
-    @R0
-    M=0
-
-    // Load R1 into D
-    @R1
-    D=M
-
-    // If R1 is 0, jump to END
-    @END
-    D;JEQ
-
-    // Load R2 into D
-    @R2
-    D=M
-
-    // If R2 is 0, jump to END
-    @END
-    D;JEQ
-
-    // Initialize loop counter
-    @i
-    M=0
+@R2
+M=0
 
 (LOOP)
-    // Check if i == R2
-    @i
-    D=M
-    @R2
-    D=D-M
-    @END
-    D;JEQ
+// Check loop condition. Quit if R0 <= 0.
+@R0
+D=M
+@END
+D;JLE
 
-    // Add R1 to R0
-    @R1
-    D=M
-    @R0
-    M=M+D
+// Update loop condition.
+@R0
+M=M-1
 
-    // Increment i
-    @i
-    M=M+1
+// Do the repeated addition.
+@R1
+D=M
+@R2
+M=D+M
 
-    // Jump back to LOOP
-    @LOOP
-    0;JMP
+@LOOP
+0;JMP
 
 (END)
-    @END
-    0;JMP
+@END
+0;JMP
